1210325ca0ddebc623b4f9f198c41f3e4348d4b5
annecy <annecyjiang@berkeley.edu>
2025-10-01 14:13:24 -0700

    done with hw2

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -6,43 +6,46 @@
 # Questions (Answer these in the homework2.py file as comments):
 
 # 1) What’s the difference between Git, GitHub, and Git Bash?
-
+# Git is the tool to do many tasks such as keeping track of changes to code or manage different versions. Git is a local system while GitHub is remote and allows hosting for git repositories. Git Bash is for windows users to run git commands.
 # 2) What’s the difference between the terminal and the command line?
-
+# A terminal allows interaction with a computer's operating system through a text-based interface while a command line is where you enter commands.
 # 3) How does Windows PowerShell differ from Git Bash?
-
+# Windows PowerShell uses its own language and bulit for windows. Git bash uses text and brings a Unix type environment to windows.
 # 4) What’s the difference between Anaconda, conda, and Python?
-
+# Anaconda is a distribution that includes python, conda, and a collection of pre-installed computer science packages and tools. Conda is a package and environment manager that can be used with python. Python is a programming language and is what anaconda and conda are built around.
 # 5) What is VS Code? 
-
+# VS code is an open ource application that is used to write and edit code and works with terminal.
 # 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
-
+# Jupyter is an interactive environment where you can write and run python code. Jupyter lab is more interactive with a more modern interface.
 # 7) What does ~/ mean?
-
+# ~/ represents the user's home directory.
 # 8) What’s the difference between an absolute path and a relative path?
-
+# An absolute path is the complete address of a file or directory starting from the root. A relative path shows the path relative to the current working directory. It uses . for current directory and .. for the parent directory
 # 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
-
+# absolute: /Users/annec/python_decal_fa25/course_assignments/homework2 relative: ./../python_decal_fa2025/course_assignments/homework2
 # 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
-
+# cd ..
 # 11) What would rm ./ do in your current directory? (Don’t try it!)
-
+# rm ./ removes the current directory
 # 12) What do the following commands do?
-# git add
+# git add 
+# tracks files in a staging area
 # git commit
+# saves work to local repository
 # git push
-
+# pushes work to remote repository
 # 13) What's the difference between "git add ." and "git add <file>"?
-
+# git add . saves all the files while git add <file> only saves the specified file. 
 # 14) What do "git status" and "git log -1" do?
-
+# git status is a command that tells the status of your repository. git log -1 displays information about the most recent commit.
 # 15) What’s the difference between cloning a repository and pulling from it?
-
+# Cloning a repository creates a new local copy of a remote repository while puling from it fetches changes from the remote repository and merges them into your local branch.
 # 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
-
+# the most frustrating bug was python3 not working because it stopped me from doing anything. I went to office hours to get it fixed and also changed my terminal on windows to gitbash.
 # 17) What’s a question you still have? What’s something you’re confused about?
-
+# A question I have is whether you can rename a commit after you've made it. I'm still kind of confused about all the different kinds of flags for commands.
 # 18) Tell me a fun fact!
-
+# army ants' jaws used to be used as sutures 
 # 19) Print your favorite math expression you've learned in Python so far. 
 # (Hint: Use print() and add a comment explaining what it does.)
+print (5 ** 10) # it prints the first number to the power of the second number (exponents)
