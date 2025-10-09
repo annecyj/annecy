f73a1c8017b3b97f3097a6562ad31338de2848bb
annecy <annecyjiang@berkeley.edu>
2025-10-08 17:44:47 -0700

    done with hw3

---
+++ homework3/average_vowels.py
@@ -0,0 +1,65 @@
+# File: average_vowels.py
+
+# You’re curious about the average number of vowels compared to consonants in a paragraph.
+
+# --- 1. Counting Vowels ---
+# Write a return function that takes a string as input.
+# The function should return a tuple containing:
+#     (number of vowels, number of consonants)
+# Name this function: counting_vowels_and_consonants()
+
+def counting_vowels_and_consonants(word):
+    vowels = "aeiou"
+    numbers_of_vowels = 0
+    number_of_consonants = 0
+   
+    for i in word:
+        if i.isalpha() == True:
+            if i in vowels:
+                numbers_of_vowels += 1
+            else:
+                number_of_consonants += 1
+    return (numbers_of_vowels, number_of_consonants)
+
+# print(counting_vowels_and_consonants("hi this is oski."))
+
+# Hint: You can use .isalpha() to check if a character is a letter.
+
+# --- 2. Average Vowels ---
+# Write a return function that takes in a paragraph (string) as input.
+# The function should:
+#   - Split the paragraph into individual sentences.
+#   - Use counting_vowels_and_consonants() to count values for each sentence.
+#   - Return a tuple: (number of sentences, average vowels per sentence, average consonants per sentence)
+# Name this function: average_vowels_and_consonants()
+
+# Here is your paragraph to analyze. It is a quote from Richard Feynman. 
+paragraph = (
+    "Fall in love with some activity, and do it! "
+    "Nobody ever figures out what life is all about, and it doesn't matter. "
+    "Explore the world. "
+    "Nearly everything is really interesting if you go into it deeply enough. "
+    "Work as hard and as much as you want to on the things you like to do the best. "
+    "Don't think about what you want to be, but what you want to do. "
+    "Keep up some kind of a minimum with other things so that society doesn't stop you from doing anything at all."
+)
+
+# Write descriptive print statements, with f-strings, that output the average vowels and consonants per sentence of the paragraph. 
+
+def average_vowels_and_consonants(paragraph):
+    punctuation = ".?!"
+    number_of_sentences = 0
+    for i in paragraph:
+         if i in punctuation:
+            number_of_sentences += 1
+    (total_vowels, total_consonants) = counting_vowels_and_consonants(paragraph)
+    average_vowels = total_vowels / number_of_sentences
+    average_consonants = total_consonants / number_of_sentences
+    return (number_of_sentences, average_vowels, average_consonants)
+
+x = average_vowels_and_consonants(paragraph)
+number_of_sentences = x[0]
+average_vowels = x[1]
+average_consonants = x[2]
+print(f"There are {number_of_sentences} sentences in the paragraph. For every sentence, there is an average of {average_vowels} vowels, and {average_consonants} consonants.")
+
